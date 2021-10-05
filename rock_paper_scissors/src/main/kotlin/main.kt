260a4376b4436a4cb7473c8e4475120c4103a01f
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-10-05 17:36:57 +0200

    rock paper scissors

---
+++ rock_paper_scissors/src/main/kotlin/main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val options = arrayOf("rock", "paper", "scissors")
+    val computerChoice = options.random()
+    val userChoice =
+
+    println("Computer picked $computerChoice")
+    println("User picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do {
+        println("Make your choice: rock - paper scissors")
+        userChoice = redLine()
+    } while (!options.contains(userChoice))
+    return userChoice
+}
+
+fun determineWinner(computerChoice: String userChoice: String) {
+    if (computerChoice == userChoice){
+        println("It's a draw")
+        return
+    }
+    val winMessage = "You have won!"
+    val loseMessage = "You have lost"
+
+    when (computerChoice) {
+        rock -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        paper -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        scissors ->
+    }
+}
