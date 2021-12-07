15173afe0a9b6e2ef80b8791cab818ac4c6a7227
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-12-07 20:13:54 +0100

    session8

--- session8/duolingo/src/main/kotlin/main.kt
+++ session8/duolingo/src/main/kotlin/main.kt
@@ -1,4 +1,11 @@
 fun main() {
     Duolingo().play()
     val Duolingo = Duolingo(5, "English")
+    try {
+    // code that can throw exception
+    } catch( e: Exception) { // e =  exception
+
+    } finally {
+    // finally block code
+    }
 }
